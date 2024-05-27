1dcfc37a076bd51fd69c49c926288011b37927ef
Abdelrady <ahmedmc95@gmail.com.com>
2024-05-27 11:55:49 +0300

    Final

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,20 @@
 import MobileNav from '@/components/shared/Mobilenav'
-import SideBar from '@/components/shared/Sidebar'
+import Sidebar from '@/components/shared/Sidebar'
 import { Toaster } from '@/components/ui/toaster'
 
-const Layout = ({children}:{children:React.ReactNode}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='root'>
-        <SideBar/>
-        <MobileNav/>
-        <div className='root-container'>
-            <div className='wrapper'>
-                {children}
-            </div>
+    <main className="root">
+      <Sidebar />
+      <MobileNav />
+
+      <div className="root-container">
+        <div className="wrapper">
+          {children}
         </div>
-        <Toaster />
+      </div>
+      
+      <Toaster />
     </main>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,19 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPlex = IBM_Plex_Sans({
-   subsets: ["latin"],
-   weight: ["400", "500", "600", "700"],
-   variable: "--font-ibm-plex",
-  });
+import "./globals.css";
+
+const IBMPlex = IBM_Plex_Sans({ 
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
+});
 
 export const metadata: Metadata = {
   title: "Imaginify",
-  description: "Ai-powered image recognition",
+  description: "AI-powered image generator",
 };
 
 export default function RootLayout({
@@ -27,16 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-    appearance={{
-      variables:{colorPrimary:"#624cf5"}
-    }}
-    >
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
         </body>
-    </html>
+      </html>
     </ClerkProvider>
   );
 }
