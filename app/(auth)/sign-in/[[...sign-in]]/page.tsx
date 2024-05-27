8cfe4af5ccfd1d7620ae2869b1aea5ce731b3211
Abdelrady <ahmedmc95@gmail.com.com>
2024-05-27 06:45:27 +0300

    update

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 import React from 'react'
 
 const SignInPage = () => {
-    return <SignIn fallbackRedirectUrl={'/'} path="/sign-in"/>
+    return <SignIn  path="/sign-in"/>
 }
 
 export default SignInPage
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from '@clerk/nextjs'
 import React from 'react'
 
 const SignUpPage = () => {
-  return <SignUp fallbackRedirectUrl ={'/'} path="/sign-up"/>
+  return <SignUp  path="/sign-up"/>
 }
 
 export default SignUpPage
